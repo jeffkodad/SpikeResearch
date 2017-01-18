15e06293d0badeccaf5f03edddda4fe46f24ccae
jeffkodad <jkodad@dontpaniclabs.com>
2017-01-18 11:00:58 -0600

    Solution Init

--- SpikeResearch.GoogleDocsAccessor/Properties/AssemblyInfo.cs
+++ SpikeResearch.GoogleDocsAccessor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Client")]
+[assembly: AssemblyTitle("SpikeResearch.GoogleDocsAccessor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Client")]
+[assembly: AssemblyProduct("SpikeResearch.GoogleDocsAccessor")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("51e0275d-a3f2-444a-923c-7c86b3267ad9")]
+[assembly: Guid("b641d317-760a-46c5-93a6-2b83d902c9f1")]
 
 // Version information for an assembly consists of the following four values:
 //
